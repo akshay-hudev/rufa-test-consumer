e85f11a783f69dfb411d643707c7012088211df6
Akshay H M <akshayhudevmanjunath@gmail.com>
2026-07-14 16:18:32 +0530

    Consume formatCurrency through middleware

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import { formatCurrency, slugify, truncateText as trunc } from "rufa-test-lib";
+import { slugify, truncateText as trunc } from "rufa-test-lib";
+import { formatCurrency } from "rufa-test-middleware";
 
 console.log(formatCurrency(1234.5));
 console.log(slugify("Hello World"));
