4f1d5af311afdf22c1757e84e503411681800838
Akshay H M <akshayhudevmanjunath@gmail.com>
2026-07-14 16:09:42 +0530

    Use truncateText through an aliased import

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import { formatCurrency, slugify } from "rufa-test-lib";
+import { formatCurrency, slugify, truncateText as trunc } from "rufa-test-lib";
 
 console.log(formatCurrency(1234.5));
 console.log(slugify("Hello World"));
+console.log(trunc("some string", 8));
